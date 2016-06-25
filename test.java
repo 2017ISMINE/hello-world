2e21b516873d7669fd13b134354e747b73935373
2017ISMINE <353660688@qq.com>
2016-06-25 18:26:41 +0800

    test.java

---
+++ test.java
@@ -0,0 +1,18 @@
+public class test{
+	static{
+		i=0;
+		System.out.print(i);
+	}
+	static int i=1;
+	
+	private int m;
+	public int intc(){
+		return m+1;
+	}
+	public static void main(String[] arg){
+		int a;
+		System.out.println(a);
+
+		System.out.print(25&7);
+	}
+}
